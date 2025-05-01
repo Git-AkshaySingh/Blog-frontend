a46f3b727a58f11d439936099f2d5ced7243f5d3
MrTech101 <mr.tech101.live@gmail.com>
2025-05-01 16:23:06 +0530

    Updated the uri for api for post route as well

--- src/components/AddPostForm.js
+++ src/components/AddPostForm.js
@@ -10,7 +10,7 @@ const AddPostForm = ({ onPostAdded }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/add_post", {
+      const res = await axios.post("https://blog-backend-lngb.onrender.com/api/posts", {
         title,
         content,
       });
